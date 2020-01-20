4824c6d3a7269385a0a12f2c29791607010d66fe
gwnm <gwnmagee@gmail.com>
2020-01-20 20:50:14 +0000

    Second draft

--- script.js
+++ script.js
@@ -1,156 +1,139 @@
-const screen = document.querySelector('#screen');
+const divScreen = document.querySelector('#div-screen');
+const btnReset = document.querySelector('#button-reset');
+const btnColor = document.querySelector('#button-toggle-colors');
 
-let color = 0;
-
-//create pixel grid
-
-function createGrid(size) {
-
-    document.styleSheets[1].cssRules[3].style["grid-template-columns"] = "repeat(" + size + ", 1fr)"
-
-    document.styleSheets[1].cssRules[3].style["grid-template-rows"] = "repeat(" + size + ", 1fr)"
-
-    for (i = 0; i < size * size; i++) {
-
-        let pixel = document.createElement('div');
-
-        pixel.className = 'pixel';
-
-        screen.appendChild(pixel);
-
-    }
-
-}
-
-//load starting grid
-
+//The default grid size is 16x16
 createGrid(16);
 
+/*
+Creates the correct amount of divs by first assigning the user-provided number  of columns and rows to the parent div, Each loop creates a div, assigns a class, then appends the div to its parent element.
+*/
+function createGrid(number) {
+  divScreen.setAttribute('style', 'grid-template: repeat('+ number + ', 1fr)' + ' \/ ' + 'repeat(' + number + ', 1fr)');
 
+  for(let i = 0; i < number * number; i++) {
+    const divBox = document.createElement('div');
 
-//add hover event listeners
+    divBox.setAttribute('class', 'box');
 
-function onHover() {
-
-    let pixels = Array.from(document.querySelectorAll(".pixel"));
-
-    // loop through each pixel adding listener
-
-    for (i = 0; i < pixels.length; i++) {
-
-        pixels[i].addEventListener('mouseover', function (e) {
-
-            //change css value based on current value
-
-            (this.style.opacity) ? this.style.opacity = parseFloat(this.style.opacity) + 0.1: this.style.opacity = 0.1;
-
-        });
-
-    }
-
+    divScreen.appendChild(divBox);
+  }
 }
-//apply event listeners to inital grid
 
-onHover();
+//Helper function for the random color generator that follows.
+function random(number) { 
+  return Math.floor(Math.random() * (number + 1));
+}
 
-// clear board on click
+/*
+Generates a random color, then applies that color to the currently chosen div.
+*/
+divScreen.addEventListener('mouseover', (event) => { 
+  const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  const currentBox = event.target;
 
-document.querySelector("#left-knob").addEventListener('click', e => {
+  /*
+  This if() statment prevents changing the background color of divScreen.
+  It's needed because of the 5px border between the parent element(divScreen)
+  and the target elements.
+  */
+  if(currentBox.id ==='div-screen') { 
+    return;
 
-    let newsize = Number(window.prompt('please provide a new grid size', 16));
-
-    // only if new size is provided create remove old grid
-
-    if (newsize) {
-
-        while (screen.hasChildNodes()) {
-
-            screen.removeChild(screen.lastChild);
-
-        }
-
-    //create new grid from size    
-
-    createGrid(newsize);
-
-    onHover();
-
-    (color) ? colourful(): "";
-
-    }
-
-});
-
-// toggle B/W and color pixels 
-
-document.querySelector('#right-knob').addEventListener('click', e => {
-
-    if (color) {
-
-        color = 0;
-
-        bw();
-
+  } else {
+    if(divScreen.className === 'colorful') {
+      currentBox.style.backgroundColor = randomColor;
+      
     } else {
-
-        color = 1;
-
-        colourful();
-
+      fadeEffect(currentBox);
     }
-
+  }
 });
 
-//add color to pixels
-
-function colourful() {
-
-    let pixels = Array.from(document.querySelectorAll(".pixel"));
-
-    const hexSymbol = "0123456789abcdef"
-
-
-
-    // loop through each pixel
-
-    for (i = 0; i < pixels.length; i++) {
-
-        let color = "#";
-
-        pixels[i].style.opacity = 0;
-
-        //create a random hexcode for each pixel
-
-        for (var j = 0; j < 6; j++) {
-
-            color += hexSymbol.charAt(Math.floor(Math.random() * hexSymbol.length));
-
-        }
-
-        //set pixel to new color
-
-        console.log(Math.floor(Math.random() * hexSymbol.length));
-
-        pixels[i].style.backgroundColor = color;
-
+function fadeEffect(element) {
+  const currentWhiteLevel = element.getAttribute('style', 'background-color');
+  
+  //This sets the initial white level of each mouse-overed element
+  if(currentWhiteLevel === null) {
+    element.setAttribute('style', 'background-color: hsl(0, 0%, 70%)');
+    
+  //The whiteLevelModifier begins at 7 (70% white), and is lowered by 10% on each pass of the mouse
+  } else {
+    let colorMode = currentWhiteLevel.substr(18, 3);
+    let whiteLevelModifier = currentWhiteLevel.substr(29, 1);
+    
+     //Formally colorful squares will be filled with the initial white level.
+    if(colorMode === 'rgb') {
+      element.setAttribute('style', 'background-color: hsl(0, 0%, 70%)');
+     
+    } else if(whiteLevelModifier <= 7 && whiteLevelModifier >= 0) {
+       whiteLevelModifier--;
+       element.setAttribute('style', 'background-color: hsl(0, 0%, ' + whiteLevelModifier + '0%)');
     }
+  }
+}
 
-};
+btnColor.addEventListener('click', () => {
+  divScreen.classList.toggle('colorful');
+  toggleAnimation();
+});
 
+/*
+This function is needed to allow a CSS animation to play to completion before
+resetting. Without this function, the animation would only play as long as 
+the user held the mouse button down.
+*/
+function toggleAnimation() {
+  btnColor.removeAttribute('id', 'button-toggle-colors');
+  btnColor.setAttribute('id', 'button-toggle-colors-animate');
+  btnColor.style.animationPlayState = 'running';
 
+  setTimeout( () => {
+    btnColor.style.animationPlayState = 'paused';
+    btnColor.removeAttribute('id', 'button-toggle-colors-animate');
+    btnColor.setAttribute('id', 'button-toggle-colors');
+  }, 300);
+}
+/*
+The (300) here refers to the time, in milliseconds, the setTimeout function
+will wait before executing the commands within the arrow function.
+*/
 
-// remove color from pixels 
+btnReset.addEventListener('click', (event) => {
+  event.stopPropagation(); //stops this event from applying to parent element
+  promptUser();
+});
 
-function bw() {
+function promptUser() {
+  let gridSize = prompt('Give me a number up to 256 and I\'ll magically produce a number X number grid for you to draw on!', 16);
 
-    let pixels = Array.from(document.querySelectorAll(".pixel"));
+  if(gridSize === null) { // If user clicks 'cancel', prompt goes away
+    return;
+  }
 
-    for (i = 0; i < pixels.length; i++) {
+// The next line ensures that the user has input a number, other input will be rejected.
+  isNaN(gridSize) ? promptUser() : checkInput(gridSize);
+}
 
-        pixels[i].style.opacity = 0;
+/*
+This function checks the user's input and will only accept a positive, whole
+number greater than 0.
+*/
+function checkInput(number) {
+  if(number <= 0 || number % 1 !== 0 || number > 256) {
+    promptUser();
 
-        pixels[i].style.backgroundColor = "black";
+  } else {
+    resetBoard();
+    createGrid(number);
+  }
+}
 
-    }
+function resetBoard() {
+  const colorBoxes = document.querySelectorAll('.box');
 
-};
+  colorBoxes.forEach((div) => {
+    div.parentNode.removeChild(div);
+  });
+}
+
